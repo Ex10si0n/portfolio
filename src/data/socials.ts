30a1d849d9655cd7f1c569732abe8416eb42ae03
ex10si0n <yan.ex10si0n@icloud.com>
2025-03-11 22:50:56 -0700

    Add dark mode, optimize visual

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,27 +1,27 @@
-import IconTwitter from "../icons/twitter.astro";
+import IconYoutube from "../icons/youtube.astro";
 import IconGithub from "../icons/github.astro";
 import IconLinkedin from "../icons/linkedin.astro";
 import IconDiscord from "../icons/discord.astro";
 
 export const SOCIALS = [
   {
-    name: "Twitter",
-    url: "https://x.com/AndrewKuktenko",
-    icon: IconTwitter,
-  },
-  {
     name: "Github",
-    url: "https://github.com/AndrewKuktenko",
+    url: "https://github.com/ex10si0n/",
     icon: IconGithub,
   },
   {
+    name: "Discord",
+    url: "https://discord.com/users/972297007102312538",
+    icon: IconDiscord,
+  },
+  {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/andrew-kuktenko/",
+    url: "https://www.linkedin.com/in/ex10si0n/",
     icon: IconLinkedin,
   },
   {
-    name: "Discord",
-    url: "https://discord.com/users/342345918533599236",
-    icon: IconDiscord,
+    name: "YouTube",
+    url: "https://www.youtube.com/@Ex10si0n/",
+    icon: IconYoutube,
   },
 ];
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,6 +1,6 @@
 import contextCursor from "../libs/context-cursor/contextCursor";
 
-var mq = window.matchMedia("(min-width: 640px)");
+const mq = window.matchMedia("(min-width: 640px)");
 if (mq.matches) {
   contextCursor({
     radius: 25,
